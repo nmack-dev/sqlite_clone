98e5c1c99f9f42bcc75b68cfa8e936d2c57e8d97
nmack <nathan@macktribe.com>
2023-01-17 18:29:39 -0800

    Create a working Repl that allows command parsing
    Create a simple function to implement a Repl, and insert it into its own thread!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <string>
+#include <thread>
+
+void ReplExecutor()
+{
+    std::string input;
+    
+    while (true) {
+    	std::cout << "db > ";
+	std::cin >> input;
+
+	if (0 == input.compare(".exit")) {
+	    exit(EXIT_SUCCESS);
+	} else {
+	    std::cout << "Unrecognized command " << input << '.' << std::endl;
+	}
+    }
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::thread ReplThread(ReplExecutor);
+    ReplThread.join();
+
     return 0;
 }
