fc5459f7387cd90795daa5fcfbd72ee930e3bc27
nmack <nathan@macktribe.com>
2023-01-27 20:34:18 -0800

    Starting this project from scratch
    Tbh I don't really know what I'm doing yet, so gonna keep things
    simple for now. Just keeping the Repl in the app for now.

--- apps/sqlite_clone.cpp
+++ apps/sqlite_clone.cpp
@@ -1,9 +1,23 @@
-#include "ReplExecutor.h"
+#include <string>
+#include <iostream>
 
 int main()
 {
-    ReplExecutor* repl = new ReplExecutor();
-	repl->Run();
+    std::string input_string;
+
+    while (true) {
+        std::cout << "db > ";
+        std::cin >> input_string;
+
+        if (0 == input_string.compare(".exit")) {
+            exit(EXIT_SUCCESS);
+        } else if (0 >= input_string.size()) {
+            std::cout << "Error reading input";
+            exit(EXIT_FAILURE);
+        } else {
+            std::cout << "Unrecognized command " << input_string << '.';
+        }
+    }
 
     return 0;
 }
